84ac93c46f570c721d36fcf90e02c5fade09ece8
bchesneau <bchesneau@0940194f-f53a-0410-a684-d5c8e9da82a5>
2007-09-25 13:05:04 +0000

    first release

---
+++ django/comments/views.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2007 Benoit Chesneau <benoitc@metavers.net>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render_to_response
+from django.core.paginator import ObjectPaginator, InvalidPage
+from django.template import loader, RequestContext
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.contrib.contenttypes.models import ContentType
+from django.utils.http import urlquote_plus, urlquote
+
+from forms import CommentForm
+from models import Comment
+
+try:
+	COMMENT_REDIRECT_TO=settings.COMMENT_REDIRECT_TO
+except AttributeError:
+	COMMENT_REDIRECT_TO=''
+
+def add(request):
+    redirect_to=COMMENT_REDIRECT_TO
+    if request.POST:
+        form=CommentForm(request.POST)
+        if form.is_valid():
+            if 'redirect' in form.cleaned_data:
+                redirect_to=form.cleaned_data['redirect']
+            else:
+                redirect_to=COMMENT_REDIRECT_TO
+
+            if not request.user.is_authenticated():
+                msg = _("In order to post a comment you should have a friendsnippets account.")
+                msg = urlquote_plus(msg)
+                redirect_to=reverse('django_openidconsumer.views.signin') + "?next=" + redirect_to + "&msg=" + msg
+                if not request.user.is_authenticated():
+                    return HttpResponseRedirect(redirect_to)
+                    headline = models.CharField(blank=True, maxlength=100)
+                    comment = models.TextField()
+                    comment_html = models.TextField(blank=True)
+                    user = models.ForeignKey(User)
+                    content_type = models.ForeignKey(ContentType)
+                    object_id = models.IntegerField()
+                    pub_date = models.DateTimeField(blank=True, null=True)
+                    is_removed = models.BooleanField(default=False)      
+            
+            try:    
+                content_type=ContentType.objects.get(id=form.cleaned_data['content_type'])
+            except:
+                pass
+            if content_type:
+                params = {
+                    'headline':form.cleaned_data['headline'],
+                    'comment':form.cleaned_data['comment'],
+                    'user':request.user,
+                    'content_type':content_type,
+                    'object_id':form.cleaned_data['object_id'],
+                }
+                c = Comment(**params)
+                c.save()
+    return HttpResponseRedirect(redirect_to)    
+    
+def tip(request):
+    from pygments import lexers
+    tlexers=lexers.get_all_lexers()
+    c = RequestContext(request, {
+		'lexers': tlexers,
+		
+	})
+
+    t = loader.get_template('comments/tipcode.html')
+    return HttpResponse(t.render(c))
+    
+    
+def list(request):
+    pass
