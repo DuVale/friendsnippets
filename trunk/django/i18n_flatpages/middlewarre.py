edcff8459176793a0b97a76e1c53c9da76788692
bchesneau <bchesneau@0940194f-f53a-0410-a684-d5c8e9da82a5>
2007-11-02 15:11:58 +0000

    - remove unused code

--- trunk/django/i18n_flatpages/middlewarre.py
+++ trunk/django/i18n_flatpages/middlewarre.py
@@ -2,52 +2,6 @@
 from django.http import Http404
 from django.conf import settings
 
-# the accept parser to run multiple times for one user.
-_accepted = {}
-
-def get_current_language(request):
-
-    supported = dict(settings.LANGUAGES)
-    if hasattr(request, 'session'):
-        lang_code = request.session.get('django_language', None)
-        if lang_code in supported and lang_code is not None:
-            return lang_code
-    lang_code = request.COOKIES.get('django_language', None)
-    if lang_code in supported and lang_code is not None:
-        return lang_code
-
-    accept = request.META.get('HTTP_ACCEPT_LANGUAGE', None)
-    if accept is not None:
-
-        t = _accepted.get(accept, None)
-        if t is not None:
-            return t
-
-        def _parsed(el):
-            p = el.find(';q=')
-            if p >= 0:
-                lang = el[:p].strip()
-                order = int(float(el[p+3:].strip())*100)
-            else:
-                lang = el
-                order = 100
-            p = lang.find('-')
-            if p >= 0:
-                mainlang = lang[:p]
-            else:
-                mainlang = lang
-            return (lang, mainlang, order)
-
-        langs = [_parsed(el) for el in accept.split(',')]
-        langs.sort(lambda a,b: -1*cmp(a[2], b[2]))
-
-        for lang, mainlang, order in langs:
-            if lang in supported or mainlang in supported:
-                _accepted[accept] = lang
-                return lang
-
-    return settings.LANGUAGE_CODE
-
 class i18nFlatpageFallbackMiddleware(object):
     def process_response(self, request, response):
         if response.status_code != 404:
