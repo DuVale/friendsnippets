e060b058813075a05be0c784306a4cc01d47aa2c
bchesneau <bchesneau@0940194f-f53a-0410-a684-d5c8e9da82a5>
2007-11-02 15:07:32 +0000

    new module from friendsnippets. Manage i18n "static" content. basic hack around flatpages.

---
+++ trunk/django/i18n_flatpages/middlewarre.py
@@ -0,0 +1,65 @@
+﻿from django.contrib.flatpages.views import flatpage
+from django.http import Http404
+from django.conf import settings
+
+# the accept parser to run multiple times for one user.
+_accepted = {}
+
+def get_current_language(request):
+
+    supported = dict(settings.LANGUAGES)
+    if hasattr(request, 'session'):
+        lang_code = request.session.get('django_language', None)
+        if lang_code in supported and lang_code is not None:
+            return lang_code
+    lang_code = request.COOKIES.get('django_language', None)
+    if lang_code in supported and lang_code is not None:
+        return lang_code
+
+    accept = request.META.get('HTTP_ACCEPT_LANGUAGE', None)
+    if accept is not None:
+
+        t = _accepted.get(accept, None)
+        if t is not None:
+            return t
+
+        def _parsed(el):
+            p = el.find(';q=')
+            if p >= 0:
+                lang = el[:p].strip()
+                order = int(float(el[p+3:].strip())*100)
+            else:
+                lang = el
+                order = 100
+            p = lang.find('-')
+            if p >= 0:
+                mainlang = lang[:p]
+            else:
+                mainlang = lang
+            return (lang, mainlang, order)
+
+        langs = [_parsed(el) for el in accept.split(',')]
+        langs.sort(lambda a,b: -1*cmp(a[2], b[2]))
+
+        for lang, mainlang, order in langs:
+            if lang in supported or mainlang in supported:
+                _accepted[accept] = lang
+                return lang
+
+    return settings.LANGUAGE_CODE
+
+class i18nFlatpageFallbackMiddleware(object):
+    def process_response(self, request, response):
+        if response.status_code != 404:
+            return response # No need to check for a flatpage for non-404 responses.
+        try:
+            flatpage=flatpage(request, request.path)
+
+        # Return the original response if any errors happened. Because this
+        # is a middleware, we can't assume the errors will be caught elsewhere.
+        except Http404:
+            return response
+        except:
+            if settings.DEBUG:
+                raise
+            return response
---
+++ trunk/django/i18n_flatpages/views.py
@@ -0,0 +1,51 @@
+
+from django.template import loader, RequestContext
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.conf import settings
+from django.core.xheaders import populate_xheaders
+
+
+from i18n_flatpages.models import FlatPage, TranslatedFlatPage
+
+DEFAULT_TEMPLATE = 'flatpages/default.html'
+
+def flatpage(request, url):
+    """
+    Flat page view.
+
+    Models: `flatpages.flatpages`
+    Templates: Uses the template defined by the ``template_name`` field,
+        or `flatpages/default.html` if template_name is not defined.
+    Context:
+        flatpage
+            `flatpages.flatpages` object
+    """
+    if not url.startswith('/'):
+        url = "/" + url
+    f = get_object_or_404(FlatPage, url__exact=url, sites__id__exact=settings.SITE_ID)
+
+    if request.LANGUAGE_CODE and request.LANGUAGE_CODE!=settings.LANGUAGE_CODE:
+        try:
+            translated = TranslatedFlatPage.objects.get(page=f.id)
+            f.title = translated.title
+            f.content = translated.content
+        except:
+            pass
+            
+
+    # If registration is required for accessing this page, and the user isn't
+    # logged in, redirect to the login page.
+    if f.registration_required and not request.user.is_authenticated():
+        from django.contrib.auth.views import redirect_to_login
+        return redirect_to_login(request.path)
+    if f.template_name:
+        t = loader.select_template((f.template_name, DEFAULT_TEMPLATE))
+    else:
+        t = loader.get_template(DEFAULT_TEMPLATE)
+    c = RequestContext(request, {
+        'flatpage': f,
+    })
+    response = HttpResponse(t.render(c))
+    populate_xheaders(request, response, FlatPage, f.id)
+    return response
